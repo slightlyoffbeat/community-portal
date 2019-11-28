fe81c994a526f74c7545f5c68f068c47c75c467b
Dave Naluz <dave@playgroundinc.com>
2019-11-27 20:38:18 -0500

    Updated copy

--- page-events.php
+++ page-events.php
@@ -3,8 +3,8 @@
     <div class="row middle-md events__container ">
       <div class="col-md-6 events__header__text">
         <h1 class="events__title"><?php the_title() ?></h1>
-        <p class="events__text">A short paragraph about why events are great and why people should look for some near them.</p>
-        <p class="events__text">Fine and event near you below, or <a href="<?php echo add_query_arg(array('action' => 'edit'), get_site_url('','events/edit-event'))?>">create an event</a></p>
+        <p class="events__text">Ready to join the movement? Check out what's happening soon in your area. </p>
+        <p class="events__text">Explore community events near you, or <a href="<?php echo add_query_arg(array('action' => 'edit'), get_site_url('','events/edit-event'))?>">organize your own!</a></p>
       </div>
     </div>
   </div>
